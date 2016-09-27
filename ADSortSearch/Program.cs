832dffa243518237f43d6401e6d488e386b41cc3
Robert Andersson <inputfalken@gmail.com>
2016-09-28 00:04:47 +0200

    Resolves Conflict.

--- ADSortSearch/Program.cs
+++ ADSortSearch/Program.cs
@@ -10,7 +10,6 @@ namespace ADSortSearch {
             var collectionSort = new CollectionSort((int) Math.Pow(5, 10));
 
             //collectionSort.PerformBinarySearch(20);
-            var ints = new List<int>().BinarySearch(2);
 
             Console.WriteLine(collectionSort.PerformBinarySearch(10));
             Console.WriteLine(collectionSort.PerformLinearSearch(10));
@@ -19,13 +18,33 @@ namespace ADSortSearch {
 
     internal class CollectionSort {
         private int[] Ints { get; }
-        private Random Random { get; } = new Random();
+        private Random Random { get; }
 
-        public CollectionSort(int length) {
+        private const int DefaultSeedValue = -1;
+
+        public CollectionSort(int length, int seed = DefaultSeedValue) {
             Ints = Range(0, length).ToArray();
+            Random = SetRandom(seed);
         }
 
+        public CollectionSort(IEnumerable<int> collection, int seed = DefaultSeedValue) {
+            Ints = collection.ToArray();
+            Random = SetRandom(seed);
+        }
+
+        private static Random SetRandom(int seed = -1) => seed == DefaultSeedValue ? new Random() : new Random(seed);
+
+        /// <summary>
+        ///     First checks if collection is sorted, if not it will throw an exception.
+        ///     Then it perform a for loop up till repeats arg. 
+        ///     Each iteration in the for loop will perform a binary search.
+        /// </summary>
+        /// <param name="repeats"></param>
+        /// <returns>The elapsed time for all attempts</returns>
         public TimeSpan PerformBinarySearch(int repeats) {
+            // Checks that the collection is ordered.
+            if (!Ints.Zip(Ints.Skip(1), (a, b) => new {a, b}).All(p => p.a < p.b))
+                throw new Exception("Collection must be sorted");
             var startNew = Stopwatch.StartNew();
             for (var i = 0; i < repeats; i++) {
                 var lo = 0;
@@ -48,6 +67,24 @@ namespace ADSortSearch {
                 if (Ints.Any(number => number == next))
                     startNew.Stop();
             }
+            return startNew.Elapsed;
+        }
+
+        public TimeSpan PerformBubbleSort() {
+            var next = 0;
+            var startNew = Stopwatch.StartNew();
+            for (var i = 0; i < Ints.Length; i++) {
+                for (var j = 0; j < Ints.Length; j++) {
+                    var current = Ints[j];
+                    if (j != Ints.Length)
+                        next = Ints[j];
+                    if (current <= next) continue;
+                    Ints[current] = next;
+                    Ints[next] = current;
+                }
+            }
+
+            startNew.Stop();
             return startNew.Elapsed;
         }
     }
