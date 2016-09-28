c71757211e0bc398b410019946a658a71d86be86
Robert Andersson <inputfalken@gmail.com>
2016-09-28 14:38:51 +0200

    Adds a stopwatch prop and public stopwatch result prop.
    The stopwatch will be protected and use to measure time for each method.
    Result prop will contain the elapsed time from stopwatch.

--- ADSortSearch/SearchAndSortTest.cs
+++ ADSortSearch/SearchAndSortTest.cs
@@ -19,6 +19,8 @@ namespace ADSortSearch {
         protected Random Random { get; } = new Random(Seed);
         protected int[] Ints { get; }
         public abstract TimeSpan Search(int repeats);
+        protected Stopwatch Watch { get; } = new Stopwatch();
+        public TimeSpan StopWatchResult { get; set; }
 
         public Tuple<TimeSpan, int[]> BubbleSort() {
             var startNew = Stopwatch.StartNew();
