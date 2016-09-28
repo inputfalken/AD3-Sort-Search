8dbc036900bba9aa0151ea860b35e9fdb20009d6
Robert Andersson <inputfalken@gmail.com>
2016-09-28 14:07:12 +0200

    Bubble sort returns a tuple containing the array and timespan.

--- ADSortSearch/Search.cs
+++ ADSortSearch/Search.cs
@@ -5,7 +5,9 @@ using System.Linq;
 namespace ADSortSearch {
     internal abstract class Search {
         protected Search(int length, bool sorted) {
-            Ints = sorted ? Enumerable.Range(0, length).ToArray() : Enumerable.Range(0, length).Select(i => Random.Next(length)).ToArray();
+            Ints = sorted
+                ? Enumerable.Range(0, length).ToArray()
+                : Enumerable.Range(0, length).Select(i => Random.Next(length)).ToArray();
         }
 
         private const int Seed = 5;
@@ -13,7 +15,7 @@ namespace ADSortSearch {
         protected int[] Ints { get; }
         public abstract TimeSpan Sort(int repeats);
 
-        public TimeSpan BubbleSort() {
+        public Tuple<TimeSpan, int[]> BubbleSort() {
             var startNew = Stopwatch.StartNew();
             for (var i = 0; i < Ints.Length - 1; i++) {
                 for (var j = 0; j < Ints.Length - 1; j++) {
@@ -25,7 +27,7 @@ namespace ADSortSearch {
                 }
             }
             startNew.Stop();
-            return startNew.Elapsed;
+            return new Tuple<TimeSpan, int[]>(startNew.Elapsed, Ints);
         }
     }
 }
