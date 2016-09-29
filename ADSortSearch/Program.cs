b39fed3ec542870c3d6f9d178ffafbe81ec54e22
Robert Andersson <inputfalken@gmail.com>
2016-09-29 14:40:52 +0200

    Adds measurement to BubbleSort.

--- ADSortSearch/Program.cs
+++ ADSortSearch/Program.cs
@@ -13,6 +13,7 @@ namespace ADSortSearch {
         private static string Measure() {
             var binaryResults = new Queue<string>();
             var linearResults = new Queue<string>();
+            var bubbleResult = new Queue<string>();
             var stringBuilder = new StringBuilder();
             const int repeats = 100000;
             const bool sorted = true;
@@ -20,20 +21,21 @@ namespace ADSortSearch {
             for (var i = 10; i < 15; i++) {
                 var measureBinary = Measurement.CreateNew(new Binary((int) Math.Pow(powBase, i), sorted));
                 var measureLinear = Measurement.CreateNew(new Linear((int) Math.Pow(powBase, i), sorted));
+                var measureBubble = Measurement.CreateNew(new Bubble((int) Math.Pow(powBase, i), !sorted));
                 measureLinear.Start(repeats);
                 measureBinary.Start(repeats);
-                binaryResults.Enqueue(
-                    measureBinary.StopWatchResult.TotalMilliseconds.ToString(CultureInfo.InvariantCulture)
-                );
-                linearResults.Enqueue(
-                    measureLinear.StopWatchResult.TotalMilliseconds.ToString(CultureInfo.InvariantCulture)
-                );
+                measureBubble.Start(1);
+                binaryResults.Enqueue(measureBinary.StopWatchResult.ToString());
+                linearResults.Enqueue(measureLinear.StopWatchResult.ToString());
+                bubbleResult.Enqueue(measureBubble.StopWatchResult.ToString());
             }
 
-            stringBuilder.AppendLine("Binary Start: ");
+            stringBuilder.AppendLine("Binary Result: ");
             while (binaryResults.Any()) stringBuilder.AppendLine(binaryResults.Dequeue());
-            stringBuilder.AppendLine("\nLinear Start: ");
+            stringBuilder.AppendLine("\nLinear Result: ");
             while (linearResults.Any()) stringBuilder.AppendLine(linearResults.Dequeue());
+            stringBuilder.AppendLine("\nBubble Sort Result: ");
+            while (bubbleResult.Any()) stringBuilder.AppendLine(bubbleResult.Dequeue());
             return stringBuilder.ToString();
         }
     }
