734861961c3956198cbab663271930e7f970f0ed
Robert Andersson <inputfalken@gmail.com>
2016-09-29 14:28:21 +0200

    Removes bubblesort from measureclass and makes it instead implement measure.

---
+++ ADSortSearch/Bubble.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ADSortSearch {
+    class Bubble : Measurement {
+        public override int[] Search(int repeats) {
+            Watch.Start();
+            for (var repeat = 0; repeat < repeats; repeat++) {
+                for (var i = 0; i < Ints.Length - 1; i++) {
+                    for (var j = 0; j < Ints.Length - 1; j++) {
+                        if (Ints[j] > Ints[j + 1]) {
+                            var temp = Ints[j];
+                            Ints[j] = Ints[j + 1];
+                            Ints[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            StopWatchResult = Watch.Elapsed;
+            Watch.Reset();
+            return Ints;
+        }
+
+
+        public Bubble(int length, bool sorted) : base(length, sorted) {}
+        public Bubble(IEnumerable<int> enumerable) : base(enumerable) {}
+    }
+}
--- ADSortSearch/Measurement.cs
+++ ADSortSearch/Measurement.cs
@@ -22,22 +22,6 @@ namespace ADSortSearch {
         protected Stopwatch Watch { get; } = new Stopwatch();
         public TimeSpan StopWatchResult { get; protected set; }
 
-        public int[] BubbleSort() {
-            Watch.Start();
-            for (var i = 0; i < Ints.Length - 1; i++) {
-                for (var j = 0; j < Ints.Length - 1; j++) {
-                    if (Ints[j] > Ints[j + 1]) {
-                        var temp = Ints[j];
-                        Ints[j] = Ints[j + 1];
-                        Ints[j + 1] = temp;
-                    }
-                }
-            }
-            StopWatchResult = Watch.Elapsed;
-            Watch.Reset();
-            return Ints;
-        }
-
         public static Measurement CreateSearch(Measurement measurement) => measurement;
     }
 }
