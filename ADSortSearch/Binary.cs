1d76bc94520fcf89d2377ff8eea9d3eccdb90f97
Robert Andersson <inputfalken@gmail.com>
2016-10-07 22:30:29 +0200

    Binary Search now requires a ilist as arugment to make method pure

--- ADSortSearch/Binary.cs
+++ ADSortSearch/Binary.cs
@@ -16,16 +16,16 @@ namespace ADSortSearch {
             if (!Collection.Zip(Collection.Skip(1), (a, b) => new {a, b}).All(p => p.a <= p.b))
                 throw new Exception("Collection must be sorted");
             watch.Start();
-            for (var i = 0; i < NumbersToFind.Length; i++) BinarySearch(NumbersToFind[i]);
+            for (var i = 0; i < NumbersToFind.Length; i++) BinarySearch(NumbersToFind[i], Collection.ToArray());
         }
 
-        private int BinarySearch(int key) {
+        private static int BinarySearch(int key, IList<int> list) {
             var lo = 0;
-            var hi = Collection.Count - 1;
+            var hi = list.Count - 1;
             while (lo <= hi) {
                 var mid = lo + (hi - lo)/2;
-                if (key < IntArray[mid]) hi = mid - 1;
-                else if (key > IntArray[mid]) lo = mid + 1;
+                if (key < list[mid]) hi = mid - 1;
+                else if (key > list[mid]) lo = mid + 1;
                 else return mid;
             }
             throw new Exception("No Result found");
