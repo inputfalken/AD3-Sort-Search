a8b79279ce0fd03ad64494b2cc9aa018ca5160e7
Robert Andersson <inputfalken@gmail.com>
2016-10-07 15:22:33 +0200

    inverted if case for contains

--- ADSortSearch/HashSetContains.cs
+++ ADSortSearch/HashSetContains.cs
@@ -9,9 +9,7 @@ namespace ADSortSearch {
         protected override void Measure(Stopwatch watch) {
             watch.Start();
             for (var i = 0; i < NumbersToFind.Length; i++) {
-                if (Collection.Contains(NumbersToFind[i]))
-                    return;
-                throw new Exception("Could not find element");
+                if (!Collection.Contains(NumbersToFind[i])) throw new Exception("Could not find element");
             }
         }
     }
