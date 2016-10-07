9fe608dda037e2bb5bb7cfbcbca9d92551ef30c2
Robert Andersson <inputfalken@gmail.com>
2016-10-07 22:31:04 +0200

    Linear now requires a ilist argument

--- ADSortSearch/Linear.cs
+++ ADSortSearch/Linear.cs
@@ -7,16 +7,14 @@ namespace ADSortSearch {
     public class Linear : Measurement {
         public Linear(ICollection<int> collection, bool sorted) : base(collection, sorted) {}
 
-        private int[] IntArr { get; set; }
 
         protected override void Measure(Stopwatch watch) {
-            IntArr = Collection.ToArray();
             watch.Start();
-            for (var i = 0; i < NumbersToFind.Length; i++) LinearSearch(NumbersToFind[i]);
+            for (var i = 0; i < NumbersToFind.Length; i++) LinearSearch(NumbersToFind[i], Collection.ToArray());
         }
 
-        private int LinearSearch(int key) {
-            for (var i = 0; i < IntArr.Length; i++) if (key == IntArr[i]) return key;
+        private static int LinearSearch(int key, IList<int> list) {
+            for (var i = 0; i < list.Count; i++) if (key == list[i]) return key;
             throw new Exception("Could not find an element.");
         }
     }
